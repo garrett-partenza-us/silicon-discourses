d54b1e2375f7f44f270b9a8378e7beba12a1863f
garrett-partenza-us <partenza.g@northeastern.edu>
2022-07-06 11:02:48 -0400

    cleanup

--- utils.py
+++
@@ -1,6 +0,0 @@
-def padding(arr, seq_len, mode="right"):
-    assert mode=="right" or mode=="left", "invalid padding mode"
-    if mode=="right":
-        return F.pad(arr, (0, seq_len-arr.nelement()))
-    else:
-        return F.pad(arr, (seq_len-arr.nelement()), 0)
